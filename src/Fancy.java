42400785d06de3c6529a6f5f1088561aa84fdbb2
Anam-jafar <52188853+Anam-jafar@users.noreply.github.com>
2022-05-12 22:45:51 +0600

    Update fro userX on main Branch

--- src/Fancy.java
+++ src/Fancy.java
@@ -1,5 +1,6 @@
 public class Fancy {
     public static void main(String[] args) {
         System.out.println("This is my fancy java project");
+        System.out.println("This is the new added line");
     }
 }
